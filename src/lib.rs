385c3f249cdf7b5bb8e805e112f4d89de0cb9769
Will Hopkins <willothyh@gmail.com>
2022-05-28 00:07:31 -0700

    feat - helper functions

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,7 @@ use std::path::{ Path, PathBuf };
 use std::fs;
 
 use ini::configparser::ini::Ini;
-
-use self::WitErrorType::{
-    DebugError,
-    IOError,
-    RepoCreationError
-};
+use self::WitErrorBuilder::*;
 
 pub enum WitErrorType {
     DebugError,
@@ -27,44 +22,66 @@ impl WitError {
             message
         }
     }
+}
 
+mod WitErrorBuilder {
+    use std::error::Error;
+    use super::{WitError, WitErrorType::*};
     // For debug use only
-    pub fn generic() -> Self {
+    pub fn generic_error() -> WitError {
         WitError {
             error: DebugError,
             message: String::from("Generic debug error.")
         }
     }
 
-    pub fn io_error(message: &str) -> Self {
+    pub fn io_error(message: String) -> WitError {
         WitError {
             error: IOError,
-            message: String::from(message)
+            message
         }
+    }
+
+    pub fn repo_creation_error(message: String) -> WitError {
+        WitError {
+            error: RepoCreationError,
+            message
+        }
+    }
+
+    pub fn from_error(error: &dyn Error) -> String {
+        format!("{}", error)
     }
 }
 
-pub struct Repository<'repo> {
-    pub worktree: &'repo str,
+pub struct Repository {
+    pub worktree: PathBuf,
     pub git_dir: PathBuf,
     pub conf: Ini
 }
 
-impl<'repo> Repository<'repo> {
-    pub fn new(path: &'repo str, force: bool) -> Result<Repository, WitError> {
+impl Repository {
+    pub fn new(path: &str, force: bool) -> Result<Repository, WitError> {
         let git_dir = Path::new(path).join(".git");
         let mut config = Ini::new();
         let config_path = git_dir.join("config");
 
+        if !(force || git_dir.is_dir()) {
+            return Err(repo_creation_error(format!("{} is not a git repository", path)))
+        }
+
         if config_path.exists() && config_path.is_file() {
-            config.load(config_path.to_str().unwrap()).unwrap();
+            config.load(match config_path.to_str() {
+                Some(string) => string,
+                None => return Err(repo_creation_error(format!("Could not load config from {:?}", config_path)))
+            }).unwrap();
             Ok(Repository {
-                worktree: path,
+                worktree: PathBuf::from(path),
                 git_dir: git_dir,
                 conf: config
             })
         } else {
-            Err(WitError::new(RepoCreationError, format!("Could not create repository in {}", path)))
+            Err(repo_creation_error(format!("Could not create repository in {}", path)))
         }
     }
 
@@ -95,15 +112,52 @@ impl<'repo> Repository<'repo> {
             if path.is_dir() {
                 Ok(path)
             } else  {
-                Err(WitError::io_error(""))
+                Err(io_error(format!("{:?} is not a directory.", path)))
             }
         } else if mkdir {
             match fs::create_dir_all(&path) {
                 Ok(_) => Ok(path),
-                Err(e) => Err(WitError::io_error(e.to_string().as_str()))
+                Err(e) => Err(io_error(from_error(&e)))
             }
         } else {
-            Err(WitError::io_error(format!("Failed to create {}", path.display()).as_str()))
+            Err(io_error(format!("Failed to create {:?}", path)))
         }
+    }
+
+    fn repo_create(path: &str) -> Result<Self, WitError> {
+        let repo = Self::new(path, true)?;
+
+        if repo.worktree.exists() {
+            if !repo.worktree.is_dir() {
+                return Err(repo_creation_error(format!("{} is not a directory.", path)))
+            }
+            if let Some(_) = fs::read_dir(path).iter().next() {
+                return Err(repo_creation_error(format!("Directory {} is not empty.", path)))
+            }
+        } else {
+            if let Err(e) = fs::create_dir_all(&repo.worktree) {
+                return Err(repo_creation_error(from_error(&e)))
+            }
+        }
+        
+        // .git/description
+        if let Err(err) = fs::write(Self::repo_file(&repo, vec!["description"], true)?, "Unnamed repository; edit this file 'description' to name the repository.\n") {
+            return Err(repo_creation_error(from_error(&err)))
+        }
+
+        // .git/HEAD
+        if let Err(err) = fs::write(Self::repo_file(&repo, vec!["HEAD"], true)?, "ref: refs/heads/main\n") {
+            return Err(repo_creation_error(from_error(&err)))
+        }
+
+        if let Err(err) = fs::write(Self::repo_file(&repo, vec!["config"], true)?, Self::repo_default_config().as_str()) {
+            return Err(repo_creation_error(from_error(&err)))
+        }
+
+        Ok(repo)
+    }
+
+    fn repo_default_config() -> String {
+        String::new()
     }
 }
